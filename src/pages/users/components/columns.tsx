2126c72b33aab0686c05e661163602f45dd58a2c
Shirshak Shrestha <shirshak97@gmail.com>
2024-06-09 14:44:13 +0545

    full_name col

--- src/pages/users/components/columns.tsx
+++ src/pages/users/components/columns.tsx
@@ -1,32 +1,59 @@
 import { ColumnDef } from "@tanstack/react-table";
+import { format } from "date-fns";
 
 export const columns: ColumnDef<IUser>[] = [
   {
+    accessorKey: "full_name",
+    header: "FULL NAME",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {row.original.first_name} {row.original.middle_name}{" "}
+          {row.original.last_name}
+        </span>
+      );
+    },
+  },
+  {
     accessorKey: "email",
-    header: "Email",
+    header: "EMAIL",
     cell: ({ row }) => {
       return <span>{row.original.email}</span>;
     },
   },
   {
     accessorKey: "address",
-    header: "Address",
+    header: "ADDRESS",
     cell: ({ row }) => {
       return <span>{row.original.address}</span>;
     },
   },
   {
     accessorKey: "country",
-    header: "Country",
+    header: "COUNTRY",
     cell: ({ row }) => {
       return <span>{row.original.country}</span>;
     },
   },
   {
     accessorKey: "active",
-    header: "Status",
+    header: "STATUS",
     cell: ({ row }) => {
       return <span>{row.original.active === "1" ? "Active" : "Inactive"}</span>;
+    },
+  },
+  {
+    accessorKey: "join_date",
+    header: "JOINED",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {format(
+            (row.original.join_date as unknown as number) * 1000,
+            "yyyy-MM-dd"
+          )}
+        </span>
+      );
     },
   },
 ];
