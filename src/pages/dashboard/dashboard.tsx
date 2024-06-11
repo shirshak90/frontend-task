149c9a8f22c355323f33d0e3efa5a6017e25dcd4
Shirshak Shrestha <shirshak97@gmail.com>
2024-06-11 09:21:42 +0545

    remove console.log

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,5 +1,4 @@
 import { DashboardHeader } from "../../components/dashboard-header";
-import { users } from "../../data";
 import { SubscriptionChart } from "./components/subscriptions-chart";
 import { TotalSubscriptions } from "./components/total-subscriptions";
 import { TotalUniqueUserCountries } from "./components/total-unique-user-countries";
@@ -7,8 +6,6 @@ import { TotalUsers } from "./components/total-users";
 import { UsersChart } from "./components/users-chart";
 
 export function DashboardPage() {
-  console.log(users.find((item) => item.join_date));
-
   return (
     <div>
       <DashboardHeader title="Dashboard" />
